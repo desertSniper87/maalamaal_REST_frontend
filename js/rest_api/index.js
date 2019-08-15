019244c106a490288409eec89215c778e15a4e3c
desertsniper87 <torshobuet@gmail.com>
2019-08-15 21:37:39 +0600

    fix footer

--- js/rest_api/index.js
+++ js/rest_api/index.js
@@ -15,10 +15,6 @@ var queryForProduct = function(categoryID){
           html = Mustache.to_html(productCardTemplateHtml, {data: data.results});
           $productList.html(html);
           $('#loadingSpinner').hide();
-          if (Cookies.get('account_type')  == 'seller') {
-            console.log('Bullets dont work jon');
-            $('.card-footer').hide();
-          }
       },
       error: function (jqXHR, textStatus, errorThrown) {
           $('#loadingSpinner').hide();
@@ -217,3 +213,9 @@ $(document).ready(function(){
     })
 });
 
+$( document ).ajaxStop(function() {
+    if (Cookies.get('account_type')  == "seller") {
+      console.log('Bullets dont work jon');
+      $('.card-footer').hide();
+    }
+});
